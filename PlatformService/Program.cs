12a2351a072671c6d62d9e335de69b380c8064d3
Bence Házi <hazibence1109@gmail.com>
2023-04-09 19:10:50 +0200

    Add platforms controllers and dtos

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+PrepDb.PrepPopulation(app);
 
 app.UseHttpsRedirection();
 
